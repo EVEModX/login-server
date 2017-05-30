5453838fc15b38569f5dbb14e80fbdb0a2a2b461
Han Yuwei <hanyuwei70@qq.com>
2017-05-30 10:05:16 +0800

    u2f first commit

---
+++ u2f.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+for u2f authentication
+*/
+const u2f=require('u2f');
+
+let router=express.Router();
+
+const APP_ID="https://login.evemodx.com";
+
+function registrationChallenge(req,resp){
+	
+}
+function registrationVerify(req,resp){
+
+}
+function authenticationChallenge(req,resp){
+
+}
+function authenticationVerify(req,resp){
+
+}
+
+
